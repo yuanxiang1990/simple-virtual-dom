7376b7b38adf714a7eebe38055481dbe0900424c
yuanxiang <yuanxiang@zbj.com>
2017-11-14 10:28:05 +0800

    differChildren算法雏形

--- virtual-dom.js
+++ virtual-dom.js
@@ -8,6 +8,17 @@ var TEXT = 3;
 function type(obj) {
     return Object.prototype.toString.call(obj).match(/\[object\s*(\w+)\]/)[1];
 }
+
+function contains(a, obj) {
+    var i = a.length;
+    while (i--) {
+        if (a[i] === obj) {
+            return true;
+        }
+    }
+    return false;
+}
+
 /**
  * virtualdom节点
  * @param tagName
@@ -15,15 +26,24 @@ function type(obj) {
  * @param children
  * @constructor
  */
-function Element (tagName, props, children) {
+function Element(tagName, props, children) {
     this.tagName = tagName;
-    this.props = props||null;
-    this.children = children||[];
+    this.props = props || null;
+    this.children = children || [];
+    this.ele = this.render();
+
 }
-function sameVnode (vnode1, vnode2) {
+
+Element.prototype = {
+    render: function () {
+
+    }
+}
+
+function sameVnode(vnode1, vnode2) {
     return (
-        vnode1&&vnode2&&vnode1.tagName === vnode2.tagName &&
-        vnode1.props == vnode2.props
+        vnode1 && vnode2 && vnode1.tagName === vnode2.tagName &&
+        vnode1.props === vnode2.props
     )
 }
 
@@ -32,33 +52,74 @@ function sameVnode (vnode1, vnode2) {
  * @param oldVnode
  * @param newVnode
  */
-function differ(oldVnode,newVnode,patch) {
+function differ(oldVnode, newVnode, patch) {
     index++;
-    if(!sameVnode(oldVnode,newVnode)){
+    if (!sameVnode(oldVnode, newVnode)) {
         patch.push({
-            type:REPLACE,
-            node:newVnode,
-            index:index
+            type: REPLACE,
+            node: newVnode,
+            index: index
         });
     }
 
-    else if(type(oldVnode)=="String"&&type(newVnode)=="String"){
-        if(oldVnode!=newVnode){
+    else if (type(oldVnode) == "String" && type(newVnode) == "String") {
+        if (oldVnode != newVnode) {
             patch.push({
-                type:TEXT,
-                node:newVnode,
-                index:index
+                type: TEXT,
+                node: newVnode,
+                index: index
             });
         }
     }
 
-    else{
-       for(var i = 0;i<oldVnode.children.length;i++){
-           differ(oldVnode.children[i],newVnode.children[i],patch);
-       }
+    else {
+        for (var i = 0; i < oldVnode.children.length; i++) {
+            differ(oldVnode.children[i], newVnode.children[i], patch);
+        }
     }
 }
+
+
+function differChildren(oldChildren, newChildren) {
+    var newAdd = [], simulateArray = [];
+    for (var i = 0; i < oldChildren.length; i++) {
+        if (!contains(newChildren, oldChildren[i])) {
+            oldChildren.splice(i, 1);
+        }
+    }
+    for (var i = 0; i < newChildren.length; i++) {
+        if (!contains(oldChildren, newChildren[i])) {
+            newAdd.push(newChildren[i]);
+        }
+    }
+    simulateArray = oldChildren.concat(newAdd);
+
+    var i = 0, j = 0;//i:simulate indx j:new array index
+    while (j < newChildren.length) {
+        var newItem = newChildren[j];
+        if (simulateArray[i]) {
+            if (simulateArray[i + 1] == newItem) {
+                simulateArray.splice(i, 1);
+                j++;
+            }
+            else {
+                simulateArray.splice(i, 1, newItem);
+                j++;
+                i++;
+            }
+        }
+        else {
+            simulateArray.push(newItem);
+            i++;
+            j++;
+        }
+    }
+    return simulateArray;
+}
+
+console.log(differChildren([1, 2, 3], [1, 2, 1, 4]));
+/*
 var oldTree = new Element('div',null,[new Element("p",null,["aaa"]),new Element("a")]);
 var newTree = new Element('div',null,[new Element("a",null,["bbb"]),new Element("p")]);
 differ(oldTree,newTree,patch);
-console.log(patch);
+console.log(patch);*/
